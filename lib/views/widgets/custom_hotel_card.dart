34b99389ab8692638728cf976e90fdfb71661976
Shubhangcs <cshubhanga@gmail.com>
2024-02-06 19:57:25 +0530

    hotel card finished

--- lib/views/widgets/custom_hotel_card.dart
+++ lib/views/widgets/custom_hotel_card.dart
@@ -8,36 +8,98 @@ class HotelCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width - 10,
-          child: Center(
-            child: Stack(
-              alignment: const Alignment(0, 2.2),
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 10),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Card(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(
-                        10,
-                      ),
-                      child: Image.asset('assets/vectors/onesta.jpg'),
-                    ),
+        child: Stack(
+          alignment: const Alignment(0, 1.8),
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 10,
+              height: 200,
+              child: Card(
+                surfaceTintColor: Colors.transparent,
+                elevation: 10,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/vectors/onesta.jpg',
+                    fit: BoxFit.fill,
                   ),
                 ),
-                Container(
-                  width: MediaQuery.of(context).size.width-20,
-                  height: 100,
-                  child: Card(
-                    elevation: 5,
-                    surfaceTintColor: Colors.white,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 20,
+              height: 100,
+              child: Card(
+                surfaceTintColor: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - 100,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Onesta Bar And Restaurent.',
+                            style: GoogleFonts.varelaRound(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Moodbidri, bar and restaurent.',
+                            style: GoogleFonts.varelaRound(
+                              color: Colors.grey,
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          Container(
+                            width: 40,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                5,
+                              ),
+                              color: Colors.green.shade400,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                const Icon(Icons.star,size: 12,color: Colors.white,),
+                                Text('4.8',style: GoogleFonts.varelaRound(color: Colors.white , fontSize: 12 , fontWeight: FontWeight.bold,),)
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 50,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.amber.shade600,
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Order',
+                          style: GoogleFonts.varelaRound(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 13),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
